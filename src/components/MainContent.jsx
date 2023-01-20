78579aca2e870becf860560702e6090e1e19f2fe
jdabassett <jacobbassett@gmail.com>
2023-01-19 20:07:28 -0700

    css finished for static template

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,21 +1,31 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faImage } from "@fortawesome/free-regular-svg-icons";
+import meme from "../images/sutmm.webp";
 
 export default function MainContent() {
   return (
     <div className="mainContent-container">
       <form className="main-form-container">
         <div className="main-input-container">
-          <input className="main-input-above"></input>
-          <input className="main-input-below"></input>
+          <input
+            className="main-input-above main-text"
+            type="text"
+            placeholder="Shut up"
+          ></input>
+          <input
+            className="main-input-below main-text"
+            type="text"
+            placeholder="and take my money"
+          ></input>
         </div>
-        <button className="main-button">
+        <button className="main-button main-text">
           Get a new meme image
-          <FontAwesomeIcon icon={faImage}></FontAwesomeIcon>
+          <FontAwesomeIcon className="button-icon" icon={faImage}></FontAwesomeIcon>
         </button>
       </form>
-      <img className="main-meme" src="" alt="boogers" />
+      {/* <div className="main-meme-container"></div> */}
+        <img className="main-meme" src={meme} alt="boogers" />
     </div>
   );
 }
